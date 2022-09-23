14ae3dad96290d6fff763b58322a1a25a8567675
Arthur.chx <achaix@edenschool.fr>
2022-09-23 14:55:10 +0200

    TYPE DE VARIABLE ET OPÉRATIONS SUR LES VARIABLES

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,27 @@
-console.log("hello")
+console.log("hello");
+let age;
+age=17;
+console.log("string");
+console.log(typeof(age));
+const prenom = 'Arthur';
+console.log(typeof(prenom));
+let addition = 4 + 12;
+console.log(addition);
+let soustraction = 2 - 50;
+console.log(soustraction);
+let multiplication = 3*8;
+console.log(multiplication);
+let division = 7/2;
+console.log(division);
+let modulo = 5% 2;
+console.log(modulo);
+let test = 3>5;
+console.log(test);
+console.log(typeof(test));
+// alors les 8 opérateurs sont ; 1.< 2.<= 3. > 4.>= 5.== 6.=== 7.!= 8.!== //
+const identity = "Bonjour" + " " + prenom + " " + "vous avez" + " " + age + " " + "ans";
+console.log(typeof(identity));
+let majuscule = identity.toUpperCase();
+console.log(identity);
+console.log(majuscule);
+// j'ai copier le contenu de "identity" et l'est mis en majuscule "ToUpperCase"//
